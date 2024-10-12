3a3705c42c203bb3e00e3e5fb05bf800cec674ec
psych25 <mnafisa@ucsd.edu>
2024-10-12 15:40:43 -0700

    reorganzing

--- data_load_test.py
+++ data_load_test.py
@@ -1,10 +1,30 @@
 import pandas as pd
+from nltk.corpus import stopwords
+import string
+import nltk
 
-# Load the dataset without headers
+nltk.download('stopwords')
+
+# Load the dataset
 test_data = pd.read_csv('/Users/maissanafisa/Downloads/archive 2/test.csv', header=None)
 
 # Assign appropriate column names
-test_data.columns = ['label', 'title', 'review_text']
+test_data.columns = ['label','title','review_text']
 
-# Check the new column names and first few rows
-print(test_data.head())
+# Define stopwords
+stop_words = set(stopwords.words('english'))
+
+def clean_text_simple(text):
+    text = text.lower()
+    
+    text = text.translate(str.maketrans('', '', string.punctuation))
+    
+    text = ' '.join([word for word in text.split() if word not in stop_words])
+    
+    return text
+
+test_data['cleaned_text'] = test_data['review_text'].apply(clean_text_simple)
+
+print(test_data[['review_text', 'cleaned_text']].head())
+
+test_data.to_csv('/Users/maissanafisa/Downloads/archive 2/cleaned_test_data.csv', index=False)
--- data_load_train.py
+++ data_load_train.py
@@ -1,11 +1,30 @@
 import pandas as pd
+from nltk.corpus import stopwords
+import string
+import nltk
 
-# Load the dataset without headers
+nltk.download('stopwords')
+
+# Load the dataset
 train_data = pd.read_csv('/Users/maissanafisa/Downloads/archive 2/train.csv', header=None)
 
 # Assign appropriate column names
-train_data.columns = ['label', 'title', 'review_text']
+train_data.columns = ['label','title','review_text']
 
-# Check the new column names and first few rows
-print(train_data.head())
+# Define stopwords
+stop_words = set(stopwords.words('english'))
 
+def clean_text_simple(text):
+    text = text.lower()
+    
+    text = text.translate(str.maketrans('', '', string.punctuation))
+    
+    text = ' '.join([word for word in text.split() if word not in stop_words])
+    
+    return text
+
+train_data['cleaned_text'] = train_data['review_text'].apply(clean_text_simple)
+
+print(train_data[['review_text', 'cleaned_text']].head())
+
+train_data.to_csv('/Users/maissanafisa/Downloads/archive 2/cleaned_train_data.csv', index=False)
